5153c6d8bca4eceee801fda4876b986bc5c1f117
DavidMichalica <davidmichalica98@email.cz>
2025-05-15 20:56:50 +0200

    aa

--- src/pages/detail.jsx
+++ src/pages/detail.jsx
@@ -4,7 +4,7 @@ import { products } from '../data/products';
 
 export const DetailProduct = () => {
     const { id } = useParams(); // Získá ID z URL
-    const product = products.find(prod => prod.id === id); 
+    const product = products.find(prod => prod.id == id); 
 
     return (
         <div className="container" style={{ margin: '0 auto', padding: '20px', fontFamily: 'Arial, sans-serif' }}>
