626ec3642f7a40e3838446181451f583ffef444b
DavidMichalica <davidmichalica98@email.cz>
2025-05-15 20:03:41 +0200

    fix

--- src/data/products.js
+++ src/data/products.js
@@ -1,7 +1,7 @@
 export const products = 
 [
   {
-    id: "12856220",
+    id: 0,
     name: "ROCK MACHINE Blizz 30-29 - Gloss Polar White/Black",
     category: "Horské kolo",
     description: "Horské kolo - pánské, 29\" kola, hliníkový rám, přehazovačka rozsah převodů:1×9, 11–46 s 9 převody, odpružená přední vidlice, se zamykatelným odpružením na vidlici, hmotnost 14,1 kg",
@@ -14,7 +14,7 @@ export const products =
     image: "https://image.alza.cz/products/SPTroma1nad/SPTroma1nad.jpg"
   },
   {
-    id: "12856219",
+    id: 1,
     name: "AUTHOR Pegas 29 - šedo-matné/zelené",
     category: "Horské kolo",
     description: "Horské kolo - pánské, 29\" kola, hliníkový rám, přehazovačka Shimano ALIVIO s 9 převody, odpružená přední vidlice, se zamykatelným odpružením na vidlici, hmotnost 14,5 kg",
@@ -27,7 +27,7 @@ export const products =
     image: "https://image.alza.cz/products/SPTauth8nad/SPTauth8nad.jpg"
   },
   {
-    id: "4721318",
+    id: 2,
     name: "Sigma BC 9.16 ATS",
     category: "Cyklocomputer",
     description: "Cyklocomputer - tachometr, bezdrátový, měření rychlosti, porovnání rychostí, výpočet kalorií, aktuální čas jízdy, hodiny, ujetá vzdálenost a autostart/stop, 1,65\" displej, maximální výdrž baterie 17532 h, běžná výdrž baterie 8766 h, kompatibilní s iOS a Windows, rozměry 50 × 35 × 10 mm (V×Š×H), hmotnost 70 g, stupeň krytí IPX7, černá barva pouzdra",
@@ -39,7 +39,7 @@ export const products =
     image: "https://image.alza.cz/products/SPTif1077/SPTif1077.jpg"
   },
   {
-    id: "12867590",
+    id: 3,
     name: "Youin Kilimanjaro L",
     category: "Elektrokolo",
     description: "Elektrokolo horské 29\", pánské/dámské, max. dojezd 100 km, kapacita baterie 540 Wh, max. rychlost 25 km/h, točivý moment 80 Nm, s motorem Vinka ve středu kola, hliníkový rám, velikost rámu L, odpružená přední vidlice, hmotnost jezdce do 120 kg",
@@ -51,7 +51,7 @@ export const products =
     image: "https://image.alza.cz/products/SPTolp493/SPTolp493.jpg"
   },
   {
-    id: "9884830",
+    id: 4,
     name: "Garmin Edge 540",
     category: "GPS navigace",
     description: "GPS navigace - na kolo, úhlopříčka displeje 2,6\", rozlišení 246 × 322 px, senzor teploměr, výškoměr a barometr, snímač tepové frekvence, rychlosti a kadence, mapy ČR + Evropa, cyklofunkce: záznam trasy, tréninkový deník, aktuální rychlost, průměrná rychlost, výpočet kalorií, notifikace z mobilu, kompatibilní s elektrokoly, aktuální čas jízdy, ujetá vzdálenost, celkový čas jízdy, autostart/stop a SOS funkce, výbava: dotykový displej a přehrávač hudby, aktualizace přes mobilní telefon, příjem dopravních informací mobilním telefonem, výdrž baterie 26 h, vnitřní paměť 16 GB, kompatibilní OS Android a iOS, komunikuje přes Bluetooth, WiFi, ANT+ a USB, bezpečnostní poutko, kabel a držák součástí balení, černá barva pouzdra, rozměry 85,1 × 57,8 × 19,6 mm (V×Š×H), hmotnost 80,4 g",
@@ -63,7 +63,7 @@ export const products =
     image: "https://image.alza.cz/products/PPedg540/PPedg540.jpg"
   },
   {
-    id: "12867594",
+    id: 5,
     name: "Youin Kilimanjaro M",
     category: "Elektrokolo",
     description: "Elektrokolo horské 29\", pánské/dámské, max. dojezd 100 km, kapacita baterie 540 Wh, max. rychlost 25 km/h, točivý moment 80 Nm, s motorem Vinka ve středu kola, hliníkový rám, velikost rámu M, odpružená přední vidlice, hmotnost jezdce do 120 kg",
@@ -75,7 +75,7 @@ export const products =
     image: "https://image.alza.cz/products/SPTolp492/SPTolp492.jpg"
   },
   {
-    id: "6734315",
+    id: 6,
     name: "Sigma ROX 4.0 černá",
     category: "Cyklocomputer",
     description: "Cyklocomputer - tachometr, bezdrátový, teploměr, výškoměr, digitální kompas (magnetický senzor) a barometr, záznam trasy, tréninkový deník, notifikace z mobilu, kompatibilní s elektrokoly a hodiny, 2,40\" LED podsvícený displej, maximální výdrž baterie 25 h, nabíjecí konektor USB-C, kompatibilní s Android, iOS a Windows, připojení přes Bluetooth, ANT+ a USB, navigace GPS, rozměry 73,6 × 44,9 × 18,4 mm (V×Š×H), hmotnost 52 g, stupeň krytí IPX7, černá barva pouzdra, držák na kolo v balení",
@@ -85,7 +85,7 @@ export const products =
     image: "https://image.alza.cz/products/SPTsig064/SPTsig064.jpg"
   },
   {
-    id: "5464108",
+    id: 7,
     name: "Fenix BC25R",
     category: "Světlo na kolo",
     description: "Světlo na kolo přední, světelný tok 600 lm, dosvit 106 m, napájení Dobíjecí akumulátor, maximální výdrž 36 h, upevnění objímkou",
@@ -95,7 +95,7 @@ export const products =
     image: "https://image.alza.cz/products/SPTkn2121/SPTkn2121.jpg"
   },
   {
-    id: "12751363",
+    id: 8,
     name: "Škoda jízdní kolo Škoda MTB vel. L",
     category: "Horské kolo",
     description: "Horské kolo - pro muže i ženy, velikost rámu L, 29\" kola, rekreační, hliníkový rám, přehazovačka Shimano Cues SL-U6000 RapidFire Plus s 10 převody, odpružená přední vidlice, se zamykatelným odpružením na vidlici, pro výšku postavy 175–185 cm, nosnost 110 kg, hmotnost 13,3 kg",
@@ -107,7 +107,7 @@ export const products =
     image: "https://image.alza.cz/products/MOTO52268/MOTO52268.jpg"
   },
   {
-    id: "6372193",
+    id: 9,
     name: "BOSCH Hustilka AKU EasyPump 0.603.947.000 (1x3 Ah)",
     category: "Kompresor",
     description: "Kompresor - akumulátorová, pro nafukování pneumatik, míčů a dalšího vybavení, výkon až 10,3 barů, nabíjení přes USB-C, LED světlo, průběžné měření, automatické zastavení, displej, součástí je několik adaptérů, dodávána v přepravní textilní brašně",
