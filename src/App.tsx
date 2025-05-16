75e3f77cdab5c39aecfb5fd4663fc5cb82dc3b66
DavidMichalica <davidmichalica98@email.cz>
2025-05-16 08:14:40 +0200

    add new chatbot

--- src/App.tsx
+++ src/App.tsx
@@ -104,7 +104,9 @@ function App() {
               </Routes>
             </Container>
             {/*<ChatBot flow={flow} settings={settings} />*/}
-
+            <script>
+            (function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="7oiLltXk7XGkqENEI18EB";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();
+            </script>
           </Box>
         </Router>
       </CartProvider>
